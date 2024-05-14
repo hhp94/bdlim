54b6dbcae3bdad258b2fda4ce2af7d2102eeb10d
AnderWilson <ander.wilson@colostate.edu>
2024-05-14 15:07:49 -0600

    update package documentation

--- R/bdlim-package.R
+++ R/bdlim-package.R
@@ -1,4 +1,13 @@
 #' @keywords internal
+#' @details
+#' The main function is \code{\link{bdlim4}}.
+#'
+#' @references Wilson A, Chiu YM, Hsu HL, Wright RO, Wright RJ, Coull BA (2017).
+#' “Bayesian distributed lag interaction models to identify perinatal windows
+#' of vulnerability in children's health.”
+#' \emph{Biostatistics}, 18(3), 537–552.
+#' \href{DOI: 10.1093/biostatistics/kxx002}{https://doi.org/10.1093/biostatistics/kxx002}.
+#' \href{arXiv preprint}{https://arxiv.org/abs/1612.05800}.
 "_PACKAGE"
 
 ## usethis namespace: start
