1abe7f08f879a5f67ae331446f98b957a5734e28
AnderWilson <ander.wilson@colostate.edu>
2024-05-17 21:22:09 -0600

    update load message

--- R/zzz.R
+++ R/zzz.R
@@ -10,7 +10,7 @@
     #
     meta <- packageDescription("bdlim")
     attachmsg <- paste("This is bdlim ",meta$Version,
-                       ".\nFor details: help(`bdlim-package`) or https://anderwilson.github.io/bdlim/. A vignette is available at https://anderwilson.github.io/bdlim/.",
+                       ".\n - For details see help(`bdlim-package`) or https://anderwilson.github.io/bdlim/.\n - A vignette is available at https://anderwilson.github.io/bdlim/articles/.",
                        sep="")
     packageStartupMessage(attachmsg, domain = NULL, appendLF = TRUE)
   }
