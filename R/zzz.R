5ec051408e4fafc4b838f02736295725b235f0a1
AnderWilson <ander.wilson@colostate.edu>
2024-05-17 21:02:33 -0600

    change vignette to article and shorten examples

--- R/zzz.R
+++ R/zzz.R
@@ -10,7 +10,7 @@
     #
     meta <- packageDescription("bdlim")
     attachmsg <- paste("This is bdlim ",meta$Version,
-                       ".\nFor details: help(`bdlim-package`), vignette('bdlim'), or https://anderwilson.github.io/bdlim/",
+                       ".\nFor details: help(`bdlim-package`) or https://anderwilson.github.io/bdlim/. A vignette is available at https://anderwilson.github.io/bdlim/.",
                        sep="")
     packageStartupMessage(attachmsg, domain = NULL, appendLF = TRUE)
   }
--- inst/examples/bdlim_example.R
+++ inst/examples/bdlim_example.R
@@ -21,32 +21,4 @@ sfit_sex
 # graph the estimated distributed lag functions for each group
 plot(sfit_sex)
 
-
-
-# run BDLIM with modification by Hispanic
-fit_hisp <- bdlim4(
-  y = sbd_bdlim$bwgaz,
-  exposure = sbd_bdlim[,paste0("pm25_",1:37)],
-  covars = sbd_bdlim[,c("MomPriorBMI","MomAge","race","ChildSex",
-                                      "EstMonthConcept","EstYearConcept")],
-  group = as.factor(sbd_bdlim$Hispanic),
-  df = 5,
-  nits = 5000,
-  parallel = FALSE
-)
-
-# show model comparison results
-fit_hisp
-
-# summarize results for best fitting pattern of modification
-sfit_hisp <- summary(fit_hisp)
-
-# graph the estimated distributed lag functions for each group
-plot(sfit_hisp)
-
-# summarize and force results from model with
-# subgroup-specific effects and weights
-sfit_hisp_bw <- summary(fit_hisp, model="bw")
-plot(sfit_hisp_bw)
-
 }
