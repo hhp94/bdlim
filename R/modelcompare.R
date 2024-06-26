b89ee1a28e5488105deb2e9cc5206d29d3d36729
Hung Pham <58577711+hhp94@users.noreply.github.com>
2024-06-26 10:31:45 -0400

    Fixed drop dimension bug if only one model is fitted.

--- R/modelcompare.R
+++ R/modelcompare.R
@@ -1,26 +1,22 @@
-
-
 #' Model comparison for bdlim objects
 #'
-#' @param object An object of class bdlim4 obtained from the bdlim4 function.
+#' @param object An object of class bdlim4 obtained from the [bdlim4()] function.
 #'
 #' @return A vector of model probabilities.
 #' @export
-
-modelcompare <- function(object){
-
-  #  iterations to keep.
-  iter_keep <- seq(object$nburn+1, object$nits, by=object$nthin)
+modelcompare <- function(object) {
+  # iterations to keep.
+  iter_keep <- seq(object$nburn + 1, object$nits, by = object$nthin)
 
   # make model probabilities
-  modelselect <- table(apply(object$loglik[iter_keep,],1,which.max))
+  modelselect <- table(apply(object$loglik[iter_keep, , drop = FALSE], 1, which.max))
   names(modelselect) <- colnames(object$loglik)[as.numeric(names(modelselect))]
 
   # add in any groups missing due to 0 probability
-  temp <- object$loglik[1,]
+  temp <- object$loglik[1, ]
   temp[] <- 0
   temp[names(modelselect)] <- c(modelselect)
-  temp <- unlist(temp/sum(temp))
+  temp <- unlist(temp / sum(temp))
 
   return(temp)
 }
